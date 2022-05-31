de513907401de9ff29ab13ec3873be9a74960a26
Dork <62394594+dorian-K@users.noreply.github.com>
2022-05-31 14:49:59 +0200

    Add Fullbright

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ import {
 import AutoClickerMod from './mods/AutoClicker';
 import SpeedMod from './mods/Speed';
 import JetpackMod from './mods/Jetpack';
+import FullbrightMod from './mods/Fullbright';
 
 ModManager.registerMod(SpeedMod);
 ModManager.registerMod(JetpackMod);
 ModManager.registerMod(AutoClickerMod);
+ModManager.registerMod(FullbrightMod);
